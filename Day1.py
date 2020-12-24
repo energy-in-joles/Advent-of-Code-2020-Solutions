ecb35199c3ca1336686052add18a6a1e5399e1c2
energy-in-joles <42647510+energy-in-joles@users.noreply.github.com>
2020-12-24 21:59:31 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,25 @@
+import csv
+import itertools
+# Interesting material: https://stackoverflow.com/questions/3420937/algorithm-to-find-which-number-in-a-list-sum-up-to-a-certain-number
+# Challenge: given a list of integers, find the product of the integers that sum to 2020. part1: 2 integers that sum, part 2: 3 integers that sum.
+
+with open('input.txt', 'r') as file: #append all numbers to a list
+  lst = [int(row.strip()) for row in file]
+
+# part 1: Using nested for loop
+for i in range(len(lst)):
+  for j in range(i+1, len(lst)): # start at i + 1 to prevent duplicate pairs ie. 1 2 and 2 1.
+    if lst[i] + lst[j] == 2020:
+        print(lst[i] * lst[j])
+
+# part 2: Using Itertools
+def prod_of_val_sum_n(sum_n, lst, val_count): # find product of [val_count] number of integers in [lst] that add to [sum_n]
+  prod = 1
+  for x in itertools.combinations(lst, val_count):
+    if sum(x) == sum_n:
+      for i in range(val_count):
+        prod *= x[i]
+  print(prod)
+
+prod_of_val_sum_n(2020, lst, 3)
+#prod_of_val_sum_n(2020, lst, 2)
